6239862ccf89f966962039e132989364f0fcbb0c
Josef Reidinger <jreidinger@suse.cz>
2010-11-01 18:44:06 +0100

    add initial appliances interface

--- lib/studio_api/appliance.rb
+++ lib/studio_api/appliance.rb
@@ -1,4 +1,11 @@
+require "studio_api/resource"
 module StudioApi
   class Appliance < Resource
+    class Status < Resource
+    end
+  def status
+    Status.set_connection self.class.studio_connection
+    Status.find :one, :from => "/appliances/#{id.to_i}/status"
+  end
   end
 end
---
+++ test/appliance_test.rb
@@ -0,0 +1,43 @@
+require 'rubygems'
+require 'active_support'
+require 'active_resource/http_mock'
+require 'test/unit'
+require 'mocha'
+$:.unshift File.join( File.dirname(__FILE__),'..','lib')
+require 'studio_api/appliance'
+require 'studio_api/connection'
+
+class ApplianceTest < Test::Unit::TestCase
+
+  def respond_load name
+    IO.read(File.join(File.dirname(__FILE__),"responses",name))
+  end
+
+  def setup
+    @connection = StudioApi::Connection.new("test","test","http://localhost")
+    StudioApi::Appliance.set_connection @connection
+    appliances_out = respond_load "appliances.xml"
+    appliance_out = respond_load "appliance.xml"
+    status_out = respond_load "status.xml"
+    ActiveResource::HttpMock.respond_to do |mock|
+      mock.get "/appliances", {"Authorization"=>"Basic dGVzdDp0ZXN0"},appliances_out,200
+      mock.get "/appliances/266657", {"Authorization"=>"Basic dGVzdDp0ZXN0"},appliance_out,200
+      mock.get "/appliances/266657/status", {"Authorization"=>"Basic dGVzdDp0ZXN0"},status_out,200
+    end
+  end
+
+  def test_find_all
+    res = StudioApi::Appliance.find :all
+    assert_equal 7,res.size
+  end
+
+  def test_find_one
+    res = StudioApi::Appliance.find 266657
+    assert_equal "266657", res.id
+  end
+
+  def test_status
+    res = StudioApi::Appliance.find 266657
+    assert_equal "ok", res.status.state
+  end
+end
