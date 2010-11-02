77354642e886f568bb509bb74eadb7201fbedaed
Josef Reidinger <jreidinger@suse.cz>
2010-11-02 15:10:19 +0100

    add class for repository searching and importing

--- lib/studio_api/repository.rb
+++ lib/studio_api/repository.rb
@@ -1,5 +1,19 @@
 require "studio_api/resource"
 module StudioApi
+  #method find :all has optional parameters base_system and filter
   class Repository < Resource
+    undef_method :save #save is useless there
+
+    def self.import (url, name)
+      response = post '',:url => url, :name => name
+      attrs = Hash.from_xml response.body
+      Repository.new attrs["repository"]
+    end
+
+#handle special studio collection method for import
+    def self.custom_method_collection_url(method_name, options = {})
+      prefix_options, query_options = split_options(options)
+      "#{prefix(prefix_options)}#{collection_name}#{query_string(query_options)}"
+    end
   end
 end
--- test/repository_test.rb
+++ test/repository_test.rb
@@ -9,7 +9,7 @@ require 'studio_api/appliance'
 require 'studio_api/connection'
 
 class ApplianceTest < Test::Unit::TestCase
-
+REPOSITORY_ID = 6343
   def respond_load name
     IO.read(File.join(File.dirname(__FILE__),"responses",name))
   end
@@ -17,14 +17,25 @@ class ApplianceTest < Test::Unit::TestCase
     @connection = StudioApi::Connection.new("test","test","http://localhost")
     StudioApi::Repository.set_connection @connection
     repositories_out = respond_load "repositories.xml"
+    repository_out = respond_load "repository.xml"
     ActiveResource::HttpMock.respond_to do |mock|
-      mock.get "/appliances/#{APPLIANCE_ID}/repositories", {"Authorization"=>"Basic dGVzdDp0ZXN0"},repositories_out,200
+      mock.get "/repositories?base_system=sle11sp1", {"Authorization"=>"Basic dGVzdDp0ZXN0"},repositories_out,200
+      mock.post "/repositories?name=test&url=http%3A%2F%2Ftest", {"Authorization"=>"Basic dGVzdDp0ZXN0"},repository_out,200
+      mock.get "/repositories/#{REPOSITORY_ID}", {"Authorization"=>"Basic dGVzdDp0ZXN0"},repository_out,200
     end
   end
 
   def test_find
-    res = StudioApi::Repository.find :all, :params => {:appliance_id => APPLIANCE_ID}
+    res = StudioApi::Repository.find :all, :params => {:base_system => "sle11sp1"}
     assert_equal 5,res.size
+    res = StudioApi::Repository.find REPOSITORY_ID
+    assert_equal REPOSITORY_ID, res.id.to_i
+  end
+
+  def test_import
+    res = StudioApi::Repository.import "http://test","test"
+    assert res
+    assert_equal REPOSITORY_ID, res.id.to_i
   end
 end
 
