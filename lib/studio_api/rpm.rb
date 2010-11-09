7820c1487b6d5945d0e3c21617d9c27494c74742
Josef Reidinger <jreidinger@suse.cz>
2010-11-09 10:57:31 +0100

    add full test suite for rpms and fix minor issues

--- lib/studio_api/rpm.rb
+++ lib/studio_api/rpm.rb
@@ -3,24 +3,23 @@ require 'cgi'
 
 
 module StudioApi
-  class Rpm < ActiveResource::Base
-    def self.upload file_path, base_system, connection = nil 
-      ::File.new( path, "r" ) do |file|
-        conn = connection || studio_connection
-        GenericRequest.new(conn).post "/rpms?base_system=#{CGI.escape base_system}", :file => file
+  class Rpm < Resource
+    self.element_name = "rpm"
+    def self.upload file_path, base_system
+      ::File.open( file_path, "r" ) do |file|
+        GenericRequest.new(studio_connection).post "/rpms?base_system=#{CGI.escape base_system}", :file => file
       end
     end
 
     # Warning: Read whole file to memory
-    def self.download target_path, file_id, connection = nil
-      conn = connection || studio_connection
-      data = GenericRequest.new(conn).get "/rpms/#{file_id.to_i}/data"
+    def self.download target_path, file_id
+      data = GenericRequest.new(studio_connection).get "/rpms/#{file_id.to_i}/data"
       ::File.open(target_path, 'w') {|f| f.write(data) }
     end
 
     # instance know already studio_id of rpm
-    def download target_path, connection = nil
-      Rpm.download target_path, id, connection
+    def download target_path
+      self.class.download target_path, id
     end
   end
 end
---
+++ test/rpm_test.rb
@@ -0,0 +1,65 @@
+require 'rubygems'
+require 'active_support'
+require 'active_resource/http_mock'
+require 'mocha'
+require 'test/unit'
+require 'tempfile'
+$:.unshift File.join( File.dirname(__FILE__), '..', 'lib')
+require 'studio_api/rpm'
+require 'studio_api/connection'
+require 'studio_api/generic_request'
+
+class RpmTest < Test::Unit::TestCase
+  APPLIANCE_ID = 269186
+  RPM_ID = 27653
+
+  def respond_load name
+    IO.read(File.join(File.dirname(__FILE__), "responses", name))
+  end
+
+  def setup
+    @connection = StudioApi::Connection.new("test", "test", "http://localhost")
+    StudioApi::Rpm.studio_connection = @connection
+
+    rpms_out = respond_load "rpms.xml"
+    rpm_out = respond_load "rpm.xml"
+
+    ActiveResource::HttpMock.respond_to do |mock|
+      mock.get "/rpms?base_system=sle11_sp1", {"Authorization"=>"Basic dGVzdDp0ZXN0"}, rpms_out, 200
+      mock.get "/rpms/#{RPM_ID}", {"Authorization"=>"Basic dGVzdDp0ZXN0"}, rpm_out, 200
+      mock.delete "/rpms/#{RPM_ID}", {"Authorization"=>"Basic dGVzdDp0ZXN0"}, rpm_out, 200
+    end
+  end
+
+  def test_find
+    res = StudioApi::Rpm.find :all, :params => {:base_system => "sle11_sp1"}
+    assert_equal 48, res.size
+    res = StudioApi::Rpm.find RPM_ID
+    assert "false", res.archive
+  end
+
+  def test_delete
+    rpm = StudioApi::Rpm.find RPM_ID
+    assert rpm.destroy
+    assert StudioApi::Rpm.delete RPM_ID
+  end
+
+TEST_STRING = "My lovely testing string\n Doodla da da da nicht"
+  def test_download
+    file = Tempfile.new("/tmp")
+    StudioApi::GenericRequest.any_instance.stubs(:get).with("/rpms/#{RPM_ID}/data").returns(TEST_STRING)
+    StudioApi::Rpm.download file.path, RPM_ID
+    File.open(file.path) { |f| assert_equal TEST_STRING,f.read }
+    StudioApi::Rpm.new(:id=> RPM_ID).download file.path
+    File.open(file.path) { |f| assert_equal TEST_STRING,f.read }
+  end
+
+  def test_upload
+    file = Tempfile.new("/tmp")
+    file.write TEST_STRING
+    file.close
+    StudioApi::GenericRequest.any_instance.stubs(:post).returns("true")
+    assert StudioApi::Rpm.upload file.path, "SLE11"
+  end
+
+end
