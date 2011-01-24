c6717964e3ea26c8aaa74d80ef2df8a4786f4fc4
Josef Reidinger <jreidinger@suse.cz>
2011-01-24 17:07:59 +0100

    add ability to see details about appliance in gallery

--- lib/studio_api/gallery.rb
+++ lib/studio_api/gallery.rb
@@ -21,10 +21,18 @@ module StudioApi
       count = tree["pages"].to_i
       page = tree["current_page"].to_i
       appliances = tree["appliance"].reduce([]) do |acc,appl|
-        appl.each { |k,v| appl[k] = nil if v.empty? } #avoid empty string
+        appl.each { |k,v| appl[k] = nil if v.empty? } #avoid empty string, array or hash
         acc << Gallery::Appliance.new(appl)
       end
       return :count => count, :page => page, :appliances => appliances
+    end
+
+    def self.appliance id, version = nil
+      request_str = "/gallery/appliances/#{id.to_i}"
+      request_str += "/version/#{CGI.escape version.to_s}" if version
+      response = GenericRequest.new(studio_connection).get request_str
+      tree = XmlSimple.xml_in(response,"ForceArray" => ["format","account"])["appliance"]
+      Gallery::Appliance.new tree
     end
   end
 end
