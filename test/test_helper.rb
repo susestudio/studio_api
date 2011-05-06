5def6cc66c3c0a114db62c14a1a24139a46d8679
Flavio Castelli <fcastelli@novell.com>
2011-05-06 14:13:40 +0200

    Small code refactorying inside of test helper.
    It's now possible to specify the status of the fake responses.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,16 +9,17 @@ class Test::Unit::TestCase
   @@password = "api_password"
   private
 
-  def register_fake_response request_type, request, response
+  def register_fake_response request_type, request, response,
+                             status = ["200", "OK"]
     url = "http://#{@@username}:#{@@password}@localhost#{request}"
-    FakeWeb.register_uri(request_type, url, :body => response,
-                         :status => ["200", "OK"])
+    FakeWeb.register_uri(request_type, url, :body => response, :status => status)
   end
 
-  def register_fake_response_from_file request_type, request, response_file_name
+  def register_fake_response_from_file request_type, request, response_file_name,
+                                       status = ["200", "OK"]
     response = IO.read(File.join(File.dirname(__FILE__),"responses",
                                  response_file_name))
-    register_fake_response request_type, request, response
+    register_fake_response request_type, request, response, status
   end
 end
 
